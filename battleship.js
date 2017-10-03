a6bdf4f5438bd7bb2b7f88e68c651f4e7a49f962
KaanBaylan <31959300+KaanBaylan@users.noreply.github.com>
2017-10-03 23:01:35 +0200

    Add files via upload
    My Solution, I hope its correct :D

--- battleship.js
+++ battleship.js
@@ -18,13 +18,112 @@ $(() => {
     tr.appendTo(battleground);
   }
 
-  $('#generate').click(() => {
-    // Here you have to add your code for building a random battleground.
+  const ships = [5, 4, 3, 3, 2];
+  var check = [true, true, true, true, true];
+  var shiplength;
+  
+  var index;
+  var posR;
+  var posC;
+  var drct;
 
-    // Tip: The next line of code demonstrates how you can select a table cell
-    // using coordinates, remove CSS classes and add CSS classes. 
-    $('td[data-r="1"][data-c="1"]').removeClass('water').addClass('ship');
-    $('td[data-r="2"][data-c="1"]').removeClass('water').addClass('ship');
-    $('td[data-r="3"][data-c="1"]').removeClass('water').addClass('ship');
+  var exit = true;
+  $('#generate').click(() => {
+    for(var i = 0; i < 10; i++){
+      for(var j = 0; j < 10; j++){
+        $('td[data-r="'+i+'"][data-c="'+j+'"]').removeClass('ship').addClass('water');
+      }
+    }
+
+    exit = true;
+    check = [true, true, true, true, true];
+    while (exit) {
+      posR = Math.floor(Math.random() * 10);
+      posC = Math.floor(Math.random() * 10);
+
+      drct = Math.floor(Math.random() * 2);
+
+      index       = selectShip(check);
+      shiplength  = ships[index];
+
+
+      switch(drct){
+        case 0:
+          if(checkFitVertical(shiplength, posR, posC)){
+            check[index] = false;
+           
+            for(var i = posR; i < (posR + shiplength); i++)
+              $('td[data-r="'+i+'"][data-c="'+posC+'"]').removeClass('water').addClass('ship');
+
+          }
+          break;
+        case 1:
+          if(checkFitHorizontal(shiplength, posR, posC)){
+            check[index] = false;
+           
+            for(var i = posC; i < (posC + shiplength); i++)
+              $('td[data-r="'+posR+'"][data-c="'+i+'"]').removeClass('water').addClass('ship');
+
+          }
+          break;
+      }
+      
+
+      exit = false;
+      for (var i = 0; i < ships.length; i++) {
+        if (check[i])
+          exit = true;
+      }
+
+    }
   });
 });
+
+
+function selectShip(check) {
+  var index = Math.floor(Math.random() * 5);
+  
+  while (!check[index]) {
+    index = Math.floor(Math.random() * 5);
+  }
+  
+  return index;
+}
+
+function checkFitVertical(shiplength, posR, posC) {
+  if((posR + shiplength) < 9){
+    for(var i = posR-1; i < (posR + shiplength) + 1; i++) {
+      if($('td[data-r="'+ i +'"][data-c="' + posC + '"]').hasClass('ship'))
+        return false;
+      
+      if($('td[data-r="' + i + '"][data-c="' + (posC-1) + '"]').hasClass('ship'))
+        return false;
+      
+      if($('td[data-r="' + i + '"][data-c="' + (posC+1) + '"]').hasClass('ship'))
+        return false;   
+      
+    }
+    return true;
+  }
+
+  return false;
+}
+
+function checkFitHorizontal(shiplength, posR, posC) {
+  if((posC + shiplength) < 9){
+    for(var i = posC-1; i < (posC + shiplength + 1); i++) {
+      if($('td[data-r="'+ posR +'"][data-c="' + i + '"]').hasClass('ship'))
+        return false;
+      
+      if($('td[data-r="' + (posR-1) + '"][data-c="' + i + '"]').hasClass('ship'))
+        return false;
+    
+      if($('td[data-r="' + (posR+1) + '"][data-c="' + i + '"]').hasClass('ship'))
+        return false;
+
+    }
+    return true;
+  }
+
+  return false;
+}
